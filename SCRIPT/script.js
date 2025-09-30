7168aee572d680013e57af91cf317dac63e0e44a
Favour Mtalika <favourmtalika027@gmail.com>
2025-09-30 22:29:06 +0200

    added the light/dark mode themes

--- SCRIPT/script.js
+++ SCRIPT/script.js
@@ -1,3 +1,4 @@
+// Humberger menu
 function myFunction() {
   var nav = document.getElementById("myLinks");
   var icon = document.getElementById("menu-icon");
@@ -26,3 +27,25 @@ document.querySelectorAll("#myLinks a").forEach(link => {
     icon.classList.add("fa-bars");
   });
 });
+
+
+// Darkmode lightmode theme
+let darkmode = localStorage.getItem('darkmode')
+const themeSwitch = document.getElementById('theme-switch')
+
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkmode', 'active')
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode')
+    localStorage.setItem('darkmode', null)
+}
+
+if(darkmode === "active") enableDarkmode()
+
+themeSwitch.addEventListener("click", () => {
+    darkmode = localStorage.getItem('darkmode')
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+})
